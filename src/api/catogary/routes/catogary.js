9889d6f443dcfd4d024f5e189d4724f2fc2ed06c
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2024-04-04 17:04:14 +0530

    heyItsMe

---
+++ src/api/catogary/routes/catogary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catogary router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::catogary.catogary');
---
+++ src/api/catogary/services/catogary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catogary service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::catogary.catogary');
